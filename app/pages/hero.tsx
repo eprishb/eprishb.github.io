4b4d52a886b263dfb0c8099dad8620cf297087b5
Epris Richardson <epris@wecohospitality.com>
2023-08-05 22:17:56 -0400

    Add Hero Basic Styling

--- app/pages/hero.tsx
+++ app/pages/hero.tsx
@@ -1,22 +1,23 @@
 import type { FC } from 'react'
+import styled from 'styled-components'
 
-type HeroProps = {
-	activePage: any;
-}
+// type HeroProps = {
+// 	activePage: any;
+// }
 
-const Hero: FC<HeroProps> = ({ activePage }: HeroProps) => {
+const Hero: FC = () => {
   return (
-    <section id="hero">
-      <div>
-        <div>
-          <p className="main">Hi! I'm &Eacute;pris. I am a ... </p>
-          <h3>
+    <Jumbotron id="hero">
+      <Container>
+        <HeroContent>
+          <Para className="main">Hi! I'm &Eacute;pris. I am a ... </Para>
+          <H3>
             Technical <br />
             <span>&</span> Visual
-          </h3>
-          <p className="second">Problem Solver</p>
-          <div>
-            {/* <Button
+          </H3>
+          <Para className="second">Problem Solver</Para>
+          {/* <div>
+            <Button
               primary
               to="portfolio"
               smooth={true}
@@ -32,12 +33,67 @@ const Hero: FC<HeroProps> = ({ activePage }: HeroProps) => {
               spy={true}
               text="MORE ABOUT ME"
               activePage={activePage}
-            /> */}
-          </div>
-        </div>
-      </div>
-    </section>
+            />
+          </div> */}
+        </HeroContent>
+      </Container>
+    </Jumbotron>
   )
 }
 
 export default Hero
+
+const Jumbotron = styled.section`
+  height: 100vh;
+`
+const Container = styled.div`
+  max-width: 1100px;
+  margin: 0 50px;
+
+  @media screen and (min-width: 768px) {
+    margin: 0 150px;
+  }
+`
+
+const HeroContent = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  height: calc(100vh - 64px);
+  padding: 100px 0;
+  color: #ccdbe5;
+
+  @media screen and (max-width: 375px) {
+    button {
+      height: 50px;
+      margin-bottom: 10px;
+    }
+  }
+`
+
+const H3 = styled.h3`
+  font-family: 'Playfair Display', serif;
+  font-size: calc(48px + (96 - 48) * (100vw - 320px) / (1200 - 320));
+  font-weight: 800;
+  line-height: 1;
+
+  span {
+    font-family: 'Playfair Display', serif;
+    color: '#BF4953';
+  }
+`
+
+const Para = styled.p`
+  font-family: 'Lato', 'Helvetica', 'Arial', sans-serif;
+  font-weight: 300;
+  font-style: italic;
+
+  &.main {
+    font-size: calc(16px + (24 - 16) * (100vw - 320px) / (1200 - 320));
+  }
+
+  &.second {
+    font-size: calc(16px + (56 - 16) * (100vw - 320px) / (1200 - 320));
+    margin-bottom: 32px;
+  }
+`
