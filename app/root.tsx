6733be09c0e0c9ad3101372cda56aa4ae8cb109d
Epris Richardson <epris@wecohospitality.com>
2023-08-05 22:29:23 -0400

    Live Reload During Development Only

--- app/root.tsx
+++ app/root.tsx
@@ -34,7 +34,7 @@ export default function App() {
 			<Outlet />
 			<ScrollRestoration />
 			<Scripts />
-			<LiveReload />
+			{ process.env.NODE_ENV === "development" ?	<LiveReload /> : null }
 		</>
   );
 }
