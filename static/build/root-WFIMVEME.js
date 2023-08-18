edfaa0a8033f569a0cd0b2dc43799000f14e7ee9
Epris Richardson <33326194+eprisr@users.noreply.github.com>
2023-08-18 06:53:19 -0400

    Static Site

---
+++ static/build/root-WFIMVEME.js
@@ -0,0 +1,40 @@
+import{b as c}from"/build/_shared/chunk-AJBFY44C.js";import{d as o,g as n,h as r,i as s,j as i,m as l,n as a}from"/build/_shared/chunk-O7F4GFJQ.js";var t;var f=c`
+  body {
+    background: #202945;
+    color: #ccdbe5;
+    font-family: Lato;
+    transition: all 0.50s linear;
+		margin: 0;
+  }
+
+	button {
+		width: fit-content;
+		height: fit-content;
+		background: none;
+    border-color: unset;
+    border: unset;
+	}
+
+	button:focus {
+		outline: unset;
+	}
+
+  a {
+    color: #bf4953;
+    cursor: pointer;
+  }
+`;var d="/build/_assets/base-MMUKVLKV.css";var e=o(a()),m=()=>[...t?[{rel:"stylesheet",href:t}]:[],{rel:"stylesheet",href:"https://use.fontawesome.com/releases/v5.15.2/css/all.css",integrity:"sha384-vSIIfh2YWi9wW0r9iZe7RJPrKwp6bG+s9QZMoITbCckVJqGCCRhc+ccxNcdpHuYu",crossOrigin:"anonymous"},{rel:"stylesheet",href:d},{rel:"preconnect",href:"https://fonts.gstatic.com"},{rel:"stylesheet",href:"https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap"},{rel:"preconnect",href:"https://fonts.gstatic.com"},{rel:"stylesheet",href:"https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;0,500;0,600;0,700;0,800;0,900;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"}];function h(){return(0,e.jsxs)("html",{lang:"en",children:[(0,e.jsxs)("head",{children:[(0,e.jsx)("meta",{charSet:"utf-8"}),(0,e.jsx)("meta",{name:"viewport",content:"width=device-width,initial-scale=1"}),(0,e.jsx)(s,{}),(0,e.jsx)(r,{})]}),(0,e.jsxs)("body",{children:[(0,e.jsx)(f,{}),(0,e.jsx)(n,{}),(0,e.jsx)(l,{}),(0,e.jsx)(i,{}),null]})]})}function u(){return(0,e.jsxs)("div",{style:{textAlign:"center"},children:[(0,e.jsx)("h1",{children:"This is a catch boundary!"}),(0,e.jsx)("p",{children:(0,e.jsx)("a",{href:"/",children:"Go back home"})})]})}export{u as CatchBoundary,h as default,m as links};
+/*! Bundled license information:
+
+@remix-run/css-bundle/dist/esm/index.js:
+  (**
+   * @remix-run/css-bundle v1.19.3
+   *
+   * Copyright (c) Remix Software Inc.
+   *
+   * This source code is licensed under the MIT license found in the
+   * LICENSE.md file in the root directory of this source tree.
+   *
+   * @license MIT
+   *)
+*/
