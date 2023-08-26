ef268f2d6462060cafb77ec2e70e45d88f39e20e
Epris Richardson <33326194+eprisr@users.noreply.github.com>
2023-08-26 12:57:15 -0400

    Update Hero

--- app/sections/Hero.tsx
+++ app/sections/Hero.tsx
@@ -2,7 +2,8 @@ import { useContext } from 'react'
 import styled from 'styled-components'
 import Button from '~/components/base/Button'
 import Container from '~/components/base/Container'
-import HeroImage from '~/assets/profile_web.png'
+import HeroImage from '~/assets/hero.png'
+import WebDevImage from '~/assets/webdevvector.png'
 import { NavContext } from '~/context/NavContext'
 import { useScrollIntoView } from '~/hooks/useScrollIntoView'
 
@@ -17,29 +18,31 @@ const Hero = () => {
 
 	return (
     <Jumbotron id="hero" ref={heroRef}>
-      <Container>
+			<StyledContainer>
+				<img src={WebDevImage} alt='Vector of a Web Developer' />
         <HeroContent>
-          <Para className="main">Hi! I'm &Eacute;pris. I am a ... </Para>
+          <Para className="main">Hi! I'm &Eacute;pris. I am a</Para>
           <H3>
             Technical <br />
             <span>&</span> Visual
           </H3>
-          <Para className="second">Problem Solver</Para>
+					<Para className="second">Problem Solver</Para>
+					<Sub>I fuse innovation and creativity to<br />craft immersive digital experiences.</Sub>
           <div>
             <Button
-              onClick={() => handleScrollToSection('portfolio')}
+							onClick={() => handleScrollToSection('experience')}
+						>
+							EXPERIENCE
+            </Button>
+            <Button
+							onClick={() => handleScrollToSection('portfolio')}
+							secondary
 						>
 							VIEW MY WORK
             </Button>
-            <Button
-							onClick={() => handleScrollToSection('about')}
-							secondary
-						>
-							MORE ABOUT ME
-            </Button>
           </div>
-        </HeroContent>
-      </Container>
+				</HeroContent>
+      </StyledContainer>
     </Jumbotron>
   )
 }
@@ -48,8 +51,13 @@ export default Hero
 
 const Jumbotron = styled.section`
   height: 100vh;
-	background-image: url(${HeroImage});
-  background-size: cover;
+	// background-image: url(${HeroImage});
+  // background-size: cover;
+`
+
+const StyledContainer = styled(Container)`
+	display: flex;
+	align-items: center;
 `
 
 const HeroContent = styled.div`
@@ -58,7 +66,8 @@ const HeroContent = styled.div`
   flex-direction: column;
   height: calc(100vh - 64px);
   padding: 100px 0;
-  color: #ccdbe5;
+	margin-right: 50px;
+  color: #202945;
 
   @media screen and (max-width: 375px) {
     button {
@@ -70,7 +79,7 @@ const HeroContent = styled.div`
 
 const H3 = styled.h3`
   font-family: 'Playfair Display', serif;
-  font-size: calc(48px + (96 - 48) * (100vw - 320px) / (1200 - 320));
+  font-size: calc(48px + (60 - 48) * (100vw - 320px) / (1200 - 320));
   font-weight: 800;
   line-height: 1;
 
@@ -89,7 +98,12 @@ const Para = styled.p`
   }
 
   &.second {
-    font-size: calc(16px + (56 - 16) * (100vw - 320px) / (1200 - 320));
-    margin-bottom: 32px;
+    font-size: calc(16px + (35 - 16) * (100vw - 320px) / (1200 - 320));
+    margin-bottom: 18px;
   }
+`
+
+const Sub = styled.p`
+	line-height: 1.5em;
+	letter-spacing: .03125em;
 `
