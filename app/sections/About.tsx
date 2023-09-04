98bfce9e503606a99fd85daed4affc156648cdf2
Epris Richardson <33326194+eprisr@users.noreply.github.com>
2023-09-04 10:07:38 -0400

    Update About Layout

--- app/sections/About.tsx
+++ app/sections/About.tsx
@@ -1,5 +1,4 @@
 import styled from 'styled-components'
-import profile from '~/assets/EprisR.jpg'
 import Container from '~/components/base/Container'
 import { useScrollIntoView } from '~/hooks/useScrollIntoView'
 
@@ -9,36 +8,38 @@ const About = () => {
 	return (
 		<Section id="about" ref={aboutRef}>
 			<StyledContainer>
-				<Profile>
-					<img src={profile} alt="Epris R" style={{width: 300, height:'auto'}} />
-				</Profile>
 				<Intro>
+					<h3>About Me</h3>
 					<Greeting>Hello, I'm &Eacute;pris Richardson</Greeting>
-					<p className="intro" style={{ marginBottom: '10px' }}>I'm a software engineer based in Charlotte, NC.</p>
-					<p>As an individual with a passion for both Software Engineering and Graphic Design, I thrive in the intersection of technology and art, creating seamless digital experiences that leave a lasting impression. From an early age, I began exploring technology and discovered a natural talent for drawing. During the MySpace era, I combined these skills by customizing pages for friends, sparking my journey into graphic design. Later, I ventured back into development, merging creativity with logic once again.</p>
+					<p className='pronunciation'>(pronounced aye-pree)</p>
+					<p className="intro">I'm a software developer based in Charlotte, NC.</p>
+					<hr/>
 				</Intro>
-				<Tech>
-					<p>Software Engineering Skills</p>
-					<ul>
-						<li><span>Programming Languages:</span> Javascript, Typescript</li>
-						<li><span>Frameworks/Libraries:</span> React, Remix, ExpressJs, Lodash</li>
-						<li><span>Testing:</span> Jest, React Testing Library, Mock Service Worker (MSW)</li>
-						<li><span>Database:</span> MongoDB, Firebase</li>
-						<li><span>Version Control:</span> Git, Github, AWS</li>
-						<li><span>Web:</span> Responsive Design, Accessibility</li>
-						<li><span>Marketing:</span> HubSpot, Google Analytics, Google Search Console, Google Tag Manager</li>
-						<li><span>Other:</span> Sentry, Trello, Shortcut</li>
-					</ul>
-				</Tech>
-				<Design>
-					<p>Graphic Design Skills</p>
-					<ul>
-						<li><span>Adobe Creative Suite:</span> InDesign, Illustrator, Photoshop, XD, After Effects</li>
-						<li><span>UI/UX:</span> Figma</li>
-						<li><span>Niches:</span> Branding and Identity, Print Design, Web Design, ECommerce</li>
-						<li><span>Industries:</span> Food and Hospitality, Marketing and Branding</li>
-					</ul>
-				</Design>
+				<Details>
+					<AboutText>As an individual with a passion for both Software Development and Graphic Design, I thrive in the intersection of technology and art, creating seamless digital experiences that leave a lasting impression. From an early age, I began exploring technology and discovered a natural talent for drawing. During the MySpace era, I combined these skills by customizing pages for friends, sparking my journey into graphic design. Later, I ventured back into development, merging creativity with logic once again.</AboutText>
+					<Skill>
+						<p>Software Engineering Skills</p>
+						<ul>
+							<li><span>Programming Languages:</span> Javascript, Typescript</li>
+							<li><span>Frameworks/Libraries:</span> React, Remix, ExpressJs, Lodash</li>
+							<li><span>Testing:</span> Jest, React Testing Library, Mock Service Worker (MSW)</li>
+							<li><span>Database:</span> MongoDB, Firebase</li>
+							<li><span>Version Control:</span> Git, Github, AWS</li>
+							<li><span>Web:</span> Responsive Design, Accessibility</li>
+							<li><span>Marketing:</span> HubSpot, Google Analytics, Google Search Console, Google Tag Manager</li>
+							<li><span>Other:</span> Sentry, Trello, Shortcut</li>
+						</ul>
+					</Skill>
+					<Skill>
+						<p>Graphic Design Skills</p>
+						<ul>
+							<li><span>Adobe Creative Suite:</span> InDesign, Illustrator, Photoshop, XD, After Effects</li>
+							<li><span>UI/UX:</span> Figma</li>
+							<li><span>Niches:</span> Branding and Identity, Print Design, Web Design, ECommerce</li>
+							<li><span>Industries:</span> Food and Hospitality, Marketing and Branding</li>
+						</ul>
+					</Skill>
+				</Details>
 			</StyledContainer>
 		</Section>
 	)
@@ -50,65 +51,69 @@ const Section = styled.section `
 	display: grid;
 	place-items: center;
 	min-height: 100vh;
-	color: #ccdbe5;
+	// color: #ccdbe5;
 `
 const StyledContainer = styled(Container) `
+	display: flex;
+	justify-content: space-between;
 	> div {
 		margin-bottom: 20px;
 	}
 	
 	@media (min-width: 768px) {
-		display: grid;
-		grid-template-columns: repeat(2, 1fr);
-		grid-template-rows: max-content 1fr;
-		grid-template-areas:
-			"intro intro"
-			"tech design"
-			"tech design";
-		column-gap: 15px;
-		row-gap: 25px;
-
 		> div {
 			margin-bottom: unset;
 		}
 	}
-
-	@media (min-width: 992px) {
-		grid-template-columns: max-content repeat(2, 1fr);
-		grid-template-rows: max-content repeat(2, 1fr);
-		grid-template-areas:
-			"intro intro intro"
-			"photo tech design"
-			"photo tech design";
-	}
-
-	@media (min-width: 1200px) {
-		grid-template-areas:
-			"photo intro intro"
-			"photo tech design"
-			"photo tech design";
-	}
 `
 
-const Profile = styled.div `
-	display: none;
-	grid-area: photo;
-
-	@media (min-width: 992px) {
-		display: block;
-	}
+const Greeting = styled.h5 `
+	font-family: 'Playfair Display', serif;
+	font-size: calc(16px + (24 - 16) * (100vw - 320px) / (1200 - 320));
+	font-weight: 500;
 `
+
 const Intro = styled.div `
+	width: 50%;
 	max-width: 600px;
-	grid-area: intro;
+	align-self: center;
+
+	h3 {
+		font-weight: 500;
+		margin-bottom: 20px;
+	}
+
+	.pronunciation {
+		font-style: italic;
+	}
 
 	.intro {
 		line-height: 1.5em;
-		font-style: italic;
+		letter-spacing: .0325em;
+	}
+
+	hr {
+		width: 25%;
+		max-width: 200px;
+		margin-top: 10px;
+		border: solid 2px #bf4953;
 	}
 `
 
+const Details = styled.div`
+	display: flex;
+	flex-wrap: wrap;
+	width: 75%;
+`
+
+const AboutText = styled.p`
+	flex: 1 0 100%;
+	margin-bottom: 15px;
+`
+
 const Skill = styled.div`
+	flex: 1 1 50%;
+
 	li {
 		list-style-position: inside;
 	}
@@ -127,18 +132,4 @@ const Skill = styled.div`
 	span {
 		font-weight: bold;
 	}
-`
-
-const Tech = styled(Skill)`
-	grid-area: tech;
-`
-
-const Design = styled(Skill)`
-	grid-area: design;
-`
-
-const Greeting = styled.h5 `
-	font-family: 'Playfair Display', serif;
-	font-size: calc(16px + (24 - 16) * (100vw - 320px) / (1200 - 320));
-	font-weight: 500;
 `
